c76af34e3c3de7493a00927a3030b366038d0f8f
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2016-04-11 17:56:26 -0700

    3rd try

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 echo Copying Files
-aws s3 cp gettingstarted/staticfiles/lang-logo.png s3://limitless-savannah-19617/images/lang-logo2.png
+aws s3 cp gettingstarted/staticfiles/lang-logo.png s3://limitless-savannah-19617/images/lang-logo3.png
 
 echo Migrating Database
 
