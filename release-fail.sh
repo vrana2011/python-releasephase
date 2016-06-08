c8c421205c8cdee0d557332faed178fe63a4a3fb
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2016-06-08 09:42:38 -0700

    fail

--- release-fail.sh
+++ release-fail.sh
@@ -1,3 +1,7 @@
-echo "Copying Files"
-aws s3 cp gettingstarted/staticfiles/lang-logo2.png s3://limitless-savannah-19617/images
+#echo "Copying Files"
+#aws s3 cp gettingstarted/staticfiles/lang-logo2.png s3://limitless-savannah-19617/images
+
+sleep 1m
+
+
 
