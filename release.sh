cb81a71a3ac73adad6daae866bea845bc0711723
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2016-07-28 08:56:01 -0700

    release phase test

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 sleep 10m
-echo Copying Files
-aws s3 cp gettingstarted/staticfiles/lang-logo.png s3://limitless-savannah-19617/images/lang-logo3.png
-
-echo Migrating Database
+#echo Copying Files
+#aws s3 cp gettingstarted/staticfiles/lang-logo.png s3://limitless-savannah-19617/images/lang-logo3.png
+#
+#echo Migrating Database
 
 
