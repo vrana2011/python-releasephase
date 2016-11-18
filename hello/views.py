392d8d11d82b6c1e764f97a40a46df1f73e1f2b4
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2016-11-18 10:46:40 -0800

    blah

--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,14 @@
 import requests
 import sys
-
-sys.path.append('pycharm-debug.egg')
-
-import pydevd
 import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
 
-pydevd.settrace('locahost', port='9090', stdoutToServer=True, stderrToServer=True)
-
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES', 3))
-    #r = requests.get('http://httpbin.org/status/418')
-    #print r.text
-    #return HttpResponse('<pre>' + r.text + '</pre>')
     return HttpResponse('New Code2')
 
 def db(request):
